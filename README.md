5a9b322b7a96b6f2f66c3a9e83c928a3769acf69
Benjamin Kniffler <bkniffler@me.com>
2015-07-31 17:42:51 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,64 @@
-# sequelize-revisions
-Enable revisioning and trails on your sequelize models
+# Sequelize-Revisions
+Enables document revisions and the trails of each revision.
+The trails make use of https://github.com/kpdecker/jsdiff to compose differences that might be displayed in a nice way:
+<img src="https://raw.githubusercontent.com/kpdecker/jsdiff/master/images/node_example.png" alt="Example">
+
+## Usage
+```javascript
+var sequelize = new Sequelize(..., {
+   ...
+});
+
+var revs = require("sequelize-revisions")(sequelize, {...});
+.. define other models, such as User
+// Using after user definition
+revs.defineModels();
+```
+
+## Example output
+**Revisions**
+```csv
+"id";"model";"documentId";"revision";"document";"createdAt";"updatedAt";"userId"
+"1";"User";"1";"1";"{"isAdmin":false,"revision":1,"id":1,"username":"bkniffler1","hash":"xxx","salt":"xxx","activationKey":"6addb6480f298340","updatedAt":"2015-07-31T15:02:35.111Z","createdAt":"2015-07-31T15:02:35.111Z","resetPasswordKey":null}";"2015-07-31 17:02:35.159+02";"2015-07-31 17:02:35.159+02";""
+"2";"User";"1";"2";"{"isAdmin":true,"revision":2,"id":1,"username":"bkniffler","hash":"xxx","salt":"xxx","activationKey":"6addb6480f298340","updatedAt":"2015-07-31T15:02:35.166Z","createdAt":"2015-07-31T15:02:35.111Z","resetPasswordKey":null}";"2015-07-31 17:02:35.198+02";"2015-07-31 17:02:35.198+02";"1"
+```
+**RevisionTrails**
+```csv
+"id";"username";"document";"diff";"createdAt";"updatedAt";"userId"
+"1";"username";"{"kind":"N","path":["username"],"rhs":"bkniffler1"}";"[{"value":"bkniffler1","added":true}]";"2015-07-31 17:02:35.186+02";"2015-07-31 17:02:35.211+02";"1"
+"2";"isAdmin";"{"kind":"N","path":["isAdmin"],"rhs":false}";"[{"value":"0","added":true}]";"2015-07-31 17:02:35.187+02";"2015-07-31 17:02:35.226+02";"1"
+"3";"username";"{"kind":"E","path":["username"],"lhs":"bkniffler1","rhs":"bkniffler"}";"[{"count":9,"value":"bkniffler"},{"count":1,"removed":true,"value":"1"}]";"2015-07-31 17:02:35.244+02";"2015-07-31 17:02:35.284+02";"2"
+"4";"isAdmin";"{"kind":"E","path":["isAdmin"],"lhs":false,"rhs":true}";"[{"count":1,"removed":true,"value":"0"},{"count":1,"added":true,"value":"1"}]";"2015-07-31 17:02:35.245+02";"2015-07-31 17:02:35.281+02";"2"
+```
+## Options
+```
+var options = {
+   // Exclude fields from audit
+   exclude: ["id", "createdAt", "updatedAt"],
+   // Revision field on other documents
+   revisionAttribute: "revision",
+   // Revision model name
+   revisionModel: "Revision",
+   // RevisionTrails model
+   revisionTrailModel: "RevisionTrail",
+   // Log
+   log: console.log
+}
+```
+
+## Get the current user
+I don't know of any good implementation for this, as a workaround I inject a context object to documents I'm about to save. These get picked up by sequelize-revisions.
+```
+User.findById(id).then(function(user){
+   if(!user){
+      return next(new Error("User with id " + id + " not found"));
+   }
+
+   user.context = { user: req.user }
+   user.updateAttributes(req.body).then(function(user) {
+      res.json(user);
+   }).catch(next);
+}).catch(next);
+```
+
+
---
+++ index.js
@@ -0,0 +1,212 @@
+var Sequelize = require("sequelize");
+var diff = require("deep-diff").diff;
+var objectPath = require("object-path");
+var jsdiff = require("diff");
+var _ = require('lodash');
+
+var options = {
+   // Exclude fields from audit
+   exclude: ["id", "createdAt", "updatedAt"],
+   // Revision field on other documents
+   revisionAttribute: "revision",
+   // Revision model name
+   revisionModel: "Revision",
+   // RevisionTrails model
+   revisionTrailModel: "RevisionTrail",
+   // Log
+   log: console.log
+}
+
+module.exports = function(sequelize, options){
+   if(!options){
+      options = {};
+   }
+   if(!options.exclude){
+      options.exclude = [
+         "id", "createdAt", "updatedAt"
+      ];
+   }
+   if(!options.revisionAttribute){
+      options.revisionAttribute = "revision";
+   }
+   if(!options.revisionModel){
+      options.revisionModel = "Revision";
+   }
+   if(!options.revisionTrailModel){
+      options.revisionTrailModel = "RevisionTrail";
+   }
+   var log = options.log || console.log;
+
+   // Extend model prototype with "isRevisionable" function
+   // Call model.isRevisionable() to enable revisions for model
+   _.extend(sequelize.Model.prototype, {
+      enableRevisions: function () {
+         log("Enable revisions on " + this.name);
+         this.attributes["revision"] = {
+            type: Sequelize.INTEGER,
+            defaultValue: 0
+         }
+         this.revisionable = true;
+         this.refreshAttributes();
+
+         this.addHook("afterCreate", after);
+         this.addHook("afterUpdate", after);
+         this.addHook("beforeCreate", before);
+         this.addHook("beforeUpdate", before);
+         return this;
+      }
+   });
+
+   // Before create/update augment revision
+   var before = function(instance, opt){
+      var changedValues = instance._changed;
+      var changed = Object.keys(changedValues).some((x)=>changedValues[x] === true);
+
+      if(changed === true){
+         instance.set(options.revisionAttribute, (instance.get(options.revisionAttribute) || 0) + 1);
+      }
+   };
+
+   // After create/update store diffs
+   var after = function(instance, opt){
+      if(instance._changed[options.revisionAttribute] === true){
+         var Revision = sequelize.model(options.revisionModel);
+         var RevisionTrail = sequelize.model(options.revisionTrailModel);
+
+         var previousVersion = instance._previousDataValues;
+         var currentVersion = instance.dataValues;
+         var user = instance.context && instance.context.user ? instance.context.user.id : null;
+
+         // Get diffs
+         var diffs = getDifferences(previousVersion, currentVersion, options.exclude);
+
+         // Build revision
+         var revision = Revision.build({
+            model: opt.model.name,
+            documentId: instance.get("id"),
+            revision: instance.get(options.revisionAttribute),
+            // Hacky, but necessary to get immutable current representation
+            document: JSON.parse(JSON.stringify(currentVersion)),
+            // Get user from instance.context, hacky workaround, any better idea?
+            userId: options.userModel ? user : null
+         });
+
+         // Save revision
+         revision.save().then(function(revision){
+            // Loop diffs and create a revision-diff for each
+            diffs.forEach(function(difference){
+               var o = convertToString(difference.lhs);
+               var n = convertToString(difference.rhs);
+               var d = RevisionTrail.build({
+                  path: difference.path.join(","),
+                  document: difference,
+                  //revisionId: data.id,
+                  diff: o || n ? jsdiff.diffChars(o, n) : []
+               });
+               d.save().then(function(d){
+                  // Add diff to revision
+                  revision.addRevisionTrail(d);
+               }).catch(log);
+            });
+         }).catch(log);
+      }
+   };
+
+   return {
+      // Return defineModels()
+      defineModels: function(){
+         // Revision model
+         var Revision = sequelize.define(options.revisionModel, {
+            model: {
+               type: Sequelize.TEXT,
+               allowNull: false
+            },
+            documentId: {
+               type: Sequelize.INTEGER,
+               allowNull: false
+            },
+            revision: {
+               type: Sequelize.INTEGER,
+               allowNull: false
+            },
+            document: {
+               type: Sequelize.JSON,
+               allowNull: false
+            }
+         });
+         // RevisionTrail model
+         var RevisionTrail = sequelize.define(options.revisionTrailModel, {
+            path: {
+               type: Sequelize.TEXT,
+               allowNull: false
+            },
+            document: {
+               type: Sequelize.JSON,
+               allowNull: false
+            },
+            diff: {
+               type: Sequelize.JSON,
+               allowNull: false
+            }
+         });
+         // Set associations
+         Revision.hasMany(RevisionTrail, {
+            foreignKey: "revisionId",
+            constraints: true
+         });
+         // Associate with user if necessary
+         if (options.userModel) {
+            Revision.belongsTo(sequelize.model(options.userModel), {
+               foreignKey: "userId",
+               constraints: true
+            });
+         }
+         return Revision;
+      }
+   }
+}
+
+// Helper: Get differences between objects
+var getDifferences = function(current, next, exclude){
+   var di = diff(current, next);
+   var diffs = di ? di.map(function(i){
+      var str = JSON.stringify(i).replace("\"__data\",", "");
+      return JSON.parse(str);
+   }).filter(function(i){
+      return i.path.join(",").indexOf("_") === -1;
+   }).filter(function(i){
+      return exclude.every(function(x){return i.path.indexOf(x) === -1; });
+   }) : [];
+   if(diffs.length > 0){
+      return diffs;
+   }
+   else{
+      return null;
+   }
+}
+
+// Helper: Convert value to some senseful string representation for storage
+var convertToString = function(val){
+   if(typeof val === "undefined" || val === null){
+      return "";
+   }
+   else if(val === true){
+      return "1";
+   }
+   else if(val === false){
+      return "0";
+   }
+   else if(typeof val === "string"){
+      return val;
+   }
+   else if(!isNaN(val)){
+      return String(val) + "";
+   }
+   else if(typeof val === "object"){
+      return JSON.stringify(val) + "";
+   }
+   else if(Array.isArray(val)){
+      return JSON.stringify(val) + "";
+   }
+   return "";
+};
